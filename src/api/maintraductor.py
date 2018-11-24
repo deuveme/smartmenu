64a697016afac19b8d9a96f79fd094634844b132
rafer1998 <you@example.com>
2018-11-24 22:40:51 +0100

    traduccion con funcion

--- src/api/maintraductor.py
+++ src/api/maintraductor.py
@@ -7,23 +7,24 @@ language_translator = LanguageTranslatorV3(
     iam_apikey='EaotF6vCJtTTWQWigO6a9oi2GVTVycPCNkQxFtqzKAbn',
     url='https://gateway-syd.watsonplatform.net/language-translator/api'
 )
-texto = 'My name is raul'
-language = language_translator.identify(
-    texto).get_result()
-language2 = (json.dumps(language, indent=2))
-counter = 1
-for language in language['languages']:
-    if counter == 1:
-        s =  language['language']
-        counter = 0
 
-translation = language_translator.translate(
-    text=texto,
-    model_id= s +'-es').get_result()
-#print(json.dumps(translation, indent=2, ensure_ascii=False))
-counter = 0
-for translations in translation["translations"]:
-    print (json.dumps(translation["translations"][counter]['translation']))
-    counter = counter + 1
+def idioma(texto):
+    language = language_translator.identify(
+        texto).get_result()
+    language2 = (json.dumps(language, indent=2))
+    counter = 1
+    for language in language['languages']:
+        if counter == 1:
+            s =  language['language']
+            counter = 0
+
+    translation = language_translator.translate(
+        text=texto,
+        model_id= s +'-es').get_result()
+    #print(json.dumps(translation, indent=2, ensure_ascii=False))
+    counter = 0
+    for translations in translation["translations"]:
+        print (json.dumps(translation["translations"][counter]['translation']))
+        counter = counter + 1
 
 

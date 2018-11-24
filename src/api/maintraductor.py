3f3803cf9337a77d283fcc58034fea2d6cb8542f
rafer1998 <you@example.com>
2018-11-24 21:32:00 +0100

    traducciov1

--- src/api/maintraductor.py
+++ src/api/maintraductor.py
@@ -3,14 +3,30 @@ import json
 from watson_developer_cloud import LanguageTranslatorV3
 
 language_translator = LanguageTranslatorV3(
-    version='2.4.1',
+    version='2018-05-01',
     iam_apikey='EaotF6vCJtTTWQWigO6a9oi2GVTVycPCNkQxFtqzKAbn',
     url='https://gateway-syd.watsonplatform.net/language-translator/api'
 )
 
 
+language = language_translator.identify(
+    'Language translator translates text from one language to another').get_result()
+language2 = (json.dumps(language, indent=2))
+
+counter = 1
+for language in language['languages']:
+    if counter == 1:
+        s =  language['language']
+        counter = 0
+
 translation = language_translator.translate(
     text='Hello',
-    model_id='en-es').get_result()
-print(json.dumps(translation, indent=2, ensure_ascii=False))
+    model_id= s +'-es').get_result()
+#print(json.dumps(translation, indent=2, ensure_ascii=False))
+counter = 0
+for translations in translation["translations"]:
+    print (json.dumps(translation["translations"][counter]['translation']))
+    counter = counter + 1
+
+
 

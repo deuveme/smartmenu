b8ebf5baa361b40d2dc30d98d3e6942901b6993e
David <davidvalero01@gmail.com>
2018-11-24 18:12:37 +0100

    Structure Updated

--- src/api/maintraductor.py
+++ src/api/maintraductor.py
@@ -3,14 +3,14 @@ import json
 from watson_developer_cloud import LanguageTranslatorV3
 
 language_translator = LanguageTranslatorV3(
-    version='2.4.1',
+    version='2018-05-01',
     iam_apikey='EaotF6vCJtTTWQWigO6a9oi2GVTVycPCNkQxFtqzKAbn',
     url='https://gateway-syd.watsonplatform.net/language-translator/api'
 )
 
 
 translation = language_translator.translate(
-    text='Hello',
+    text='Hello I am David how are you?',
     model_id='en-es').get_result()
 print(json.dumps(translation, indent=2, ensure_ascii=False))
 

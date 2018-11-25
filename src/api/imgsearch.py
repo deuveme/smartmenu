b34c84af0b398424843d07b8db7a10ad56ff7e2a
Ferran Velasco <ferranvelasco98@gmail.com>
2018-11-25 07:52:00 +0100

    Buscar imagen añadido

---
+++ src/api/imgsearch.py
@@ -0,0 +1,17 @@
+import requests
+
+VISION_API_KEY = "AIzaSyAZtRpcJdd9mhTk_iMz4s5ss7O3lJwH9yM"
+
+def busca_img(query):
+    payload = {
+        "q": query,
+        "num": 1,
+        "start": 1,
+        "imgSize": "medium",
+        "searchType": "image",
+        "key": VISION_API_KEY,
+        "cx": "001729826209899299709:luisubz3cc4"
+    }
+
+    response = requests.get('https://www.googleapis.com/customsearch/v1', params=payload).json()
+    return response['items'][0]['link']
